69ddf1fa11087c0884760328c146568b9240e4fa
hostomani <hostomani@gmail.com>
2023-07-22 17:58:56 +0100

    publish PyPi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='odooless',
-    version='0.1.2',
+    version='0.1.3',
     description='A DynamoDB ORM inspired by Odoo',
     long_description='A DynamoDB ORM inspired by Odoo',
     author='Sam Hasan',
