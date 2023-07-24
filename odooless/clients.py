35ee82e1149b8c2685d2006be2e85da5f7fbbe7e
hostomani <hostomani@gmail.com>
2023-07-24 01:45:31 +0100

    publish PyPi

--- odooless/clients.py
+++ odooless/clients.py
@@ -4,7 +4,7 @@ import env
 
 aws_access_key = os.environ.get("aws_access_key")
 aws_secret_access_key = os.environ.get("aws_secret_access_key")
-region_name = os.environ.get("aws_secret_access_key")
+region_name = os.environ.get("region_name")
 
 session = boto3.Session(
     aws_access_key_id=aws_access_key_id,
