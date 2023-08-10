74a968b83bc548279e016580e8f984b2c831cf69
Hosam Hasan <hostomani@gmail.com>
2023-08-10 11:14:09 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='odooless',
-    version='0.1.16',
+    version='0.1.17',
     description='A DynamoDB ORM inspired by Odoo',
     long_description=long_description,
     long_description_content_type='text/markdown',
